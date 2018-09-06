0a7b4e3121c9f99f5115092bf092b4cc73e389c5
이다인 <dlekdls602@naver.com>
2018-09-06 22:18:29 +0900

    library추가

---
+++ ClassLibrary/Packet.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+using System.Configuration;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.Office.Core;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+
+namespace ClassLibrary
+{
+    public enum PacketType : int
+    {
+        UPLOAD = 0,
+        LOCK
+    }
+
+    [Serializable]
+    public class Packet
+    {
+        public int type;
+        public int length;
+
+        public Packet()
+        {
+            this.type = 0;
+            this.length = 0;
+        }
+        public static byte[] Serialize(Object o)
+        {
+            MemoryStream ms = new MemoryStream(1024 * 4);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(ms, o);
+            return ms.ToArray();
+        }
+        public static object Desserialize(byte[] bt)
+        {
+            MemoryStream ms = new MemoryStream(1024 * 4);
+            ms.Write(bt, 0, bt.Length);
+            ms.Position = 0;
+            BinaryFormatter bf = new BinaryFormatter();
+            Object obj = bf.Deserialize(ms);
+            ms.Close();
+            return obj;
+        }
+    }
+
+    [Serializable]
+    public class LockPacket : Packet
+    {
+        public int pageNum { get; set; }
+    }
+    [Serializable]
+    public class UploadPacket : Packet
+    {
+        public bool isup { get; set; }
+    }
+}
